8f8dd15c696803b4abbe2839ab33fb418a2474cb
shivam0295 <tripathishivam8710@gmail.com>
2021-03-22 17:42:44 +0530

    View Old Appointment Booking And Lab Booking

--- hospital.management1/src/main/java/com/newlife/hospital/management1/controller/AppoinmentController.java
+++ hospital.management1/src/main/java/com/newlife/hospital/management1/controller/AppoinmentController.java
@@ -1,10 +1,18 @@
 package com.newlife.hospital.management1.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.newlife.hospital.management1.model.Appointment;
+import com.newlife.hospital.management1.model.Patients;
 import com.newlife.hospital.management1.repositary.AppointmentRepository;
 
 @Controller
@@ -19,5 +27,13 @@ public class AppoinmentController {
 		
 		
 		return "appoinment_success";
+	}
+	@GetMapping("/oldappoinment" )
+	public String listUsers(@RequestParam(value="email",required = false) String email,Model model) {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		String name = authentication.getName();
+		List<Appointment> listUser = repo1.findById(name);
+		model.addAttribute("listUser", listUser);
+		return "appoinment_list";
 	}
 }
--- hospital.management1/src/main/java/com/newlife/hospital/management1/controller/LabController.java
+++ hospital.management1/src/main/java/com/newlife/hospital/management1/controller/LabController.java
@@ -1,8 +1,15 @@
 package com.newlife.hospital.management1.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.newlife.hospital.management1.model.Appointment;
 import com.newlife.hospital.management1.model.Lab;
@@ -20,5 +27,12 @@ public class LabController {
 		
 		return "lab_success";
 	}
-	
+	@GetMapping("/oldlab" )
+	public String listUsers(@RequestParam(value="email",required = false) String email,Model model) {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		String name = authentication.getName();
+		List<Lab> listUser1 = repo3.findById(name);
+		model.addAttribute("listUser1", listUser1);
+		return "lab_list";
+	}
 }
